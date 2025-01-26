4bc858af27c5927bc808005b68097636f44a4143
eliazynho <m10elias10@gmail.com>
2025-01-26 17:16:36 -0300

    ficando foda

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+function openSite() {
+    window.open('https://90sburgers.com.br/', '_blank');
+}
+
+function openHistory() {
+    window.open('https://90sburgers.com.br/', '_blank');
+}
+
+function openIfoos1() {
+    window.open('https://www.ifoos.com.br/90sburgers/', '_blank');
+}
+
+function openIfoos2() {
+    window.open('https://www.ifoos.com.br/90sburgers/', '_blank');
+}
+
