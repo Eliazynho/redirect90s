310ce3509aa770cf27ff6fe106057bf709e0ae45
eliazynho <m10elias10@gmail.com>
2025-01-27 16:58:57 -0300

    configurando tela

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ function openHistory() {
 }
 
 function openIfoos1() {
-    window.open('https://www.ifoos.com.br/90sburgers/', '_blank');
+    window.open('https://www.ifood.com.br/delivery/garanhuns-pe/90s-burgers-n-fries-noventas-aluisio-pinto/c7e768e6-75ae-480d-95dc-c276672066ac?UTM_Medium=share', '_blank');
 }
 
 function openIfoos2() {
-    window.open('https://www.ifoos.com.br/90sburgers/', '_blank');
+    window.open('https://www.ifood.com.br/delivery/garanhuns-pe/90s-burgers-n-fries-noventas---heliopolis-heliopolis/78a85065-b14f-4972-877f-f3c12d261d50?UTM_Medium=share', '_blank');
 }
 
